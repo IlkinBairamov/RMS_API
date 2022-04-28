0bcafaada04af1f9e021d1b705bc72b804a12109
Elvin46 <elvinva@code.edu.az>
2022-04-28 17:43:06 +0400

    All Repository Interfaces of Entities Created

---
+++ RMS.Core/Repositories/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using RMS.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMS.Core.Repositories
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+
+    }
+}
---
+++ RMS.Core/Repositories/IFoodRepository.cs
@@ -0,0 +1,13 @@
+﻿using RMS.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMS.Core.Repositories
+{
+    internal interface IFoodRepository : IRepository<Food>
+    {
+    }
+}
