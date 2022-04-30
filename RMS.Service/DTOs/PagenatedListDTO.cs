c3c6c9907afdd80e5cd307f2e6204581e0bf2874
IlkinBairamov <ilkinabr@code.edu.az>
2022-04-30 12:06:16 +0400

    Create PagenatedListDTO

---
+++ RMS.Service/DTOs/PagenatedListDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMS.Service.DTOs
+{
+    class PagenatedListDTO<T>
+    {
+        public PagenatedListDTO(IEnumerable<T> items, int pageIndex, int totalCount, int pageSize)
+        {
+            Items = items;
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
+            HasNext = pageIndex < TotalPages;
+            HasPrev = pageIndex > 1;
+        }
+
+        public IEnumerable<T> Items { get; }
+        public int PageIndex { get; }
+        public int TotalPages { get; }
+        public bool HasNext { get; }
+        public bool HasPrev { get; }
+    }
+}
